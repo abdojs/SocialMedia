c21dfff12c0393ecd61cb1988484860422cd7be4
abdojs <wat1.1taki@gmail.com>
2023-01-31 22:42:44 +0100

    create module create account

---
+++ back-end/modules/singUp/singUp.modele.js
@@ -0,0 +1,31 @@
+
+const bcrypt = require("bcrypt");
+
+
+
+const connectDB = require("../../dataBase/connect")
+
+
+
+
+
+
+
+const singUp = () => {
+
+
+
+    console.log(connectDB);
+
+
+
+
+
+
+
+
+
+
+}
+
+module.exports = singUp
