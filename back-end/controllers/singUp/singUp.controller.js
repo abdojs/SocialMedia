762983f6c0b611a29a2f24bd90b9cd6126fc9073
abdojs <wat1.1taki@gmail.com>
2023-02-01 21:31:15 +0100

    hadele functions singUp()[module] and singUp()[controller] for create account

--- back-end/controllers/singUp/singUp.controller.js
+++ back-end/controllers/singUp/singUp.controller.js
@@ -3,13 +3,45 @@
 const singUpModule = require('../../modules/singUp/singUp.modele');
 
 
-
 const singUp = async (req, res) => {
 
+    try {
+        const response = await singUpModule(req.body)
 
-    console.log(singUpModule);
+        const { type, status, message } = response
 
-    await res.status(200).json({ success: "success in send request" })
+        if (type && status === 201)
+            return res.status(201).json({
+                success: true,
+                data: message
+            })
+
+        if (!type && status === 400) {
+
+            const { code, keyValue, keyPattern, errors } = message
+
+            if (message.hasOwnProperty("errors"))
+                return res.status(400).json({
+                    success: false,
+                    data: errors
+                })
+            if (message.hasOwnProperty("code"))
+                return res.status(200).json({
+                    success: false,
+                    data: {
+                        message: `is really wrong `,
+                        keyValue,
+                        keyPattern
+                    }
+                })
+            if (!message.hasOwnProperty("code"))
+                return res.status(400).json({ success: message.hasOwnProperty("code"), error: message.message })
+
+        }
+    } catch (error) {
+        return res.status(500).json({ success: false, data: error.message })
+    }
+
 }
 
 
