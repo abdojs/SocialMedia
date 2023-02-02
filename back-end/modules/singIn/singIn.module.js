a07012dc08df98bc1994ed6faa40a224cb56ecfb
abdojs <wat1.1taki@gmail.com>
2023-02-02 20:54:11 +0100

    check for find user in login

---
+++ back-end/modules/singIn/singIn.module.js
@@ -0,0 +1,52 @@
+const { coonectDB, disConnectDB } = require("../../dataBase/connect")
+const User = require('../../schemas/singUp/schema');
+const bcrypt = require("bcrypt");
+
+const singIn = async ({ emailPhone, password }, withLogin) => {
+
+    try {
+        return await coonectDB().then(async () => {
+
+            const typeLogin = withLogin ? 'email' : 'phone';
+
+            const user = await User.findOne({ [typeLogin]: emailPhone });
+
+            disConnectDB()
+
+            if (!user)
+                return {
+                    type: false,
+                    message: "uers not found",
+                    status: 200,
+                };
+
+            const validPassword = await bcrypt.compare(password, user.password)
+
+            if (!validPassword)
+                return {
+                    type: false,
+                    message: "wrong password",
+                    status: 200,
+                };
+
+            const { email: _email, name: _name, profilePicture: _profilePicture, _id: _id } = user
+
+            return {
+                type: true,
+                message: user,
+                status: 200,
+            }
+        })
+    } catch (error) {
+
+        return {
+            type: false,
+            message: error,
+            status: 400,
+        }
+
+    }
+
+}
+
+module.exports = singIn
