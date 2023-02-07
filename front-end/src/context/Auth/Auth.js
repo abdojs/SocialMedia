001b73870ee7335d2dae691ae98119fc34b09e05
abdojs <wat1.1taki@gmail.com>
2023-02-07 11:32:20 +0100

    added prop in object INITIAL_STATE

--- front-end/src/context/Auth/Auth.js
+++ front-end/src/context/Auth/Auth.js
@@ -1,12 +1,17 @@
 import { createContext, useEffect, useReducer } from "react";
+import { getCookie } from "./Cookie"
 import Reducer from "./Reducer";
-const INITIAL_STATE = {
-    user: JSON.parse(localStorage.getItem("user")) || null,
-    isFetching: false,
-    error: false,
-};
 
-console.log(INITIAL_STATE);
+const getTokenCookie = getCookie("ct_user")
+
+const ct_user = typeof getTokenCookie === "string" && getCookie("ct_user") !== "" ? getTokenCookie : null;
+
+const INITIAL_STATE = {
+    token: ct_user || null,
+    user: null,
+    isFetching: false,
+    error: null,
+};
 
 const AuthContext = createContext(INITIAL_STATE);
 
@@ -15,12 +20,16 @@ const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
 
     useEffect(() => {
+
+        // console.log(ct_user); // fetch data and insert in local storage
+
         localStorage.setItem("user", JSON.stringify(state.user))
+
     }, [state.user])
 
     return (
         <AuthContext.Provider value={{
-            user: state.user,
+            user: state.token,
             isFetching: state.isFetching,
             error: state.error,
             dispatch,
